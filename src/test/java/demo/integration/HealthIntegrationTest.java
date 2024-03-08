820d0b4f534008b863b457a815802dcc3b2901a3
rgolder1 <rgolder1@yahoo.com>
2024-03-08 11:21:04 +0000

    Integration testing.

--- src/test/java/demo/integration/HealthIntegrationTest.java
+++ src/test/java/demo/integration/HealthIntegrationTest.java
@@ -1,20 +1,19 @@
 package demo.integration;
 
+import io.github.embeddedkafka.EmbeddedKafka;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@Disabled
 @MicronautTest
-public class HealthIntegrationTest {
+public class HealthIntegrationTest implements EmbeddedKafka {
 
     @Inject
     @Client("/")
---
+++ src/test/java/demo/integration/SingletonEmbeddedKafka.java
@@ -0,0 +1,21 @@
+package demo.integration;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ExtendWith({SingletonEmbeddedKafkaExtension.class})
+//@Repeatable(SingletonEmbeddedKafkas.class)
+public @interface SingletonEmbeddedKafka {
+    String name() default "default";
+    String bootstrapServerProperty() default "spring.kafka.bootstrap-servers";
+}
